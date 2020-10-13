14c2cf31b81222c96cc3ca9ebfaac343b27138b0
jhondoeelmisterio <69824622+jhondoeelmisterio@users.noreply.github.com>
2020-10-13 17:08:48 -0400

    Update and rename README.md to vacacional.py

---
+++ vacacional.py
@@ -0,0 +1,44 @@
+# vacaciones
+print ("Vacacion Empresarial")
+nombre = (input("Cual es tu nombre? "))
+resutado = nombre
+print ("Hola", nombre)
+print ("Empezemos con el listado")                                                                                    print ("Listado de departamentos")
+print ("Presiona 1 DEPARTAMENTO DE ATENCION AL CLIENTE")
+print ("Presiona 2 DEPARTAMENTO DE LOGISTICA")                                                                        print ("Presiona 3 GERENCIA")
+respuesta = int(input("Cual es tu departamento? "))
+
+if respuesta == 1:
+                print ("Escogiste DEPARTAMENTO DE ATENCION AL CLIENTE")
+
+                mama = int(input("Cuantos años as trabajado? "))
+
+                       elif mama == 1:
+                              print ("tienes derecho a 6 dias de descanso", nombre)
+                       elif mama == 2 or 3 or 4 or 5 or 6:                                                                                          print ("tienes derecho a 14 dias de descanso", nombre)
+                       elif mama == 7:
+                              print ("Tienes derecho a 20 dias de descanso", nombre)
+
+if respuesta == 2:
+                print ("Escogiste DEPARTAMENTO DE LOGISTICA")
+
+                papa = int(input("Cuantos años as trabajado? "))
+
+                       elif papa == 1:
+                              print ("Tienes derecho a 7 dias de descanso", nombre)
+                       elif papa == 2 or 3 or 4 or 5 or 6:
+                              print ("Tienes derecho a 15 dias de descanso", nombre)
+                       elif papa == 7:
+                              print ("Tienes derecho a 22 dias de descanso", nombre)
+
+if respuesta == 3:
+                print ("Escogiste GERENCIA")
+
+                bro = int(input("Cuantos años as trabajado? "))
+
+                       elif bro == 1:
+                              print ("Tienes derecho a 10 dias de descanso", nombre)
+                       elif bro == 2 or 3 or 4 or 5 or 6:
+                              print ("Tienes derecho a 20 dias de descanso", nombre)
+                       elif bro == 7:
+                              print ("Tienes derecho a 30 dias de descanso", nombre)
